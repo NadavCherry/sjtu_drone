3e94e698a598f3ba6d05beada9b932a2646a63ea
Nadav Cherry <nadavcherry@gmail.com>
2025-10-01 21:12:41 +0300

    add laser to the drone

--- sjtu_drone_bringup/sjtu_drone_bringup/spawn_drone.py
+++ sjtu_drone_bringup/sjtu_drone_bringup/spawn_drone.py
@@ -1,22 +1,8 @@
 #!/usr/bin/env python3
-# Copyright 2023 Georg Novotny
-#
-# Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.gnu.org/licenses/gpl-3.0.en.html
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# -*- coding: utf-8 -*-
 import sys
 import rclpy
 from gazebo_msgs.srv import SpawnEntity
+from geometry_msgs.msg import Pose
 
 
 def main(args=None):
@@ -27,11 +13,24 @@ def main(args=None):
     content = sys.argv[1]
     namespace = sys.argv[2]
 
+    # SAFER INITIAL POSITION - Inside the hospital
+    initial_pose = Pose()
+    initial_pose.position.x = 1.0    # Center of the world
+    initial_pose.position.y = 1.0    # Center of the world
+    initial_pose.position.z = 2.0    # 1 meter above ground
+
+    # Orientation (quaternion) - default is no rotation
+    initial_pose.orientation.x = 0.0
+    initial_pose.orientation.y = 0.0
+    initial_pose.orientation.z = 0.0
+    initial_pose.orientation.w = 1.0
+
     req = SpawnEntity.Request()
     req.name = namespace
     req.xml = content
     req.robot_namespace = namespace
     req.reference_frame = "world"
+    req.initial_pose = initial_pose
 
     while not cli.wait_for_service(timeout_sec=1.0):
         node.get_logger().info('service not available, waiting again...')
@@ -41,7 +40,8 @@ def main(args=None):
 
     if future.result() is not None:
         node.get_logger().info(
-            'Result ' + str(future.result().success) + " " + future.result().status_message)
+            f'Spawned at ({initial_pose.position.x}, {initial_pose.position.y}, {initial_pose.position.z}) - ' +
+            f'Result: {future.result().success} - {future.result().status_message}')
     else:
         node.get_logger().info('Service call failed %r' % (future.exception(),))
 
